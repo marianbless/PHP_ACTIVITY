3496d3009b023894a302e9057aa8728b28f1b105
Marian Bless <permentillaml@students.nu-lipa.edu.ph>
2025-06-03 21:52:41 +0800

    index.php

--- index.php
+++ index.php
@@ -1,135 +1,112 @@
 <?php
 
-$name = $address = $email = "";
-$nameErr = $addressErr = $emailErr = "";
+$email = $password = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$emailErr = $passwordErr = "";
 
-    if (empty ($_POST["name"])) {
-        $nameErr = "Name is required!";
-    } else {
-        $name = $_POST["name"];
-    }
 
-    if (empty ($_POST["address"])) {
-        $addressErr = "Address is required!";
-    } else {
-        $address = $_POST["address"];
-    }
 
-    if (empty ($_POST["email"])) {
-        $emailErr = "Email is required!";
-    } else {
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    if(empty($_POST["email"])){
+
+        $emailErr = "Email is required!";  
+
+    }else{
+
         $email = $_POST["email"];
+
     }
+
+
+     if(empty($_POST["password"])){
+
+        $emailErr = "Password is required!";  
+
+    }else{
+
+        $email = $_POST["password"];
+
+    }
+
+
+
+
+    if($email && $password) {
+
+      include("connections.php");
+
+      $check_email = mysqli_query(connections, "SELECT * FROM mytbl WHERE email='$email'");
+
+      $check_email_row = mysqli_num_rows($check_email);
+
+    if($check_email_row > 0){
+
+
+        while($row = mysqli_fetch_assoc($check_email)){
+             $user_id = $row["id"];
+
+            $db_password = $row["password"];
+            $db_account_type = $row["account_type"];
+
+                if($password == $db_password){
+
+                        session_start();
+
+
+
+                        $_SESSION["id"] = $user_id;
+
+
+                        if($db_account_type == "1"){
+
+                            echo "<script>window.location.href='admin';</script>";
+
+                        }else{
+
+                            echo "<script>window.location.href='user';</script>";
+
+                        }
+
+
+                }
+                
+
+
+        }else{
+
+            $emailErr = "Email is not registered!";
+        }       
+
+    }
+
 
 }
 
 ?>
 
+
 <style>
-.error { 
-     color:red;
+
+.error{
+    color:red;
 }
-   
+
 </style>
 
-<form method="POST" action="<?php htmlspecialchars ("PHP_SELF"); ?>">
 
-<input type="text" name= "name" value="<?php echo $name; ?>"> <br>
-<span class="error"><?php echo $nameErr; ?> </span> <br>
+<form method="POST" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 
-<input type="text" name= "address" value="<?php echo $address; ?>"> <br>
-<span class="error"><?php echo $addressErr; ?> </span> <br>
+<input type="text" name="email" value="<?php echo $Email; ?>"> <br>
+<span  class="error"><php echo $emailErr; ?></span> <br>
 
-<input type="text" name= "email" value="<?php echo $email; ?>"> <br>
-<span class="error"><?php echo $emailErr; ?> </span> <br>
 
-<input type="submit" value="Submit">
+<input type="password" name="password" value=""> <br>
+<span  class="error"><php echo $passwordErr; ?></span> <br>
+
+<input type="submit" value="Login">
+
 
 </form>
 
-<hr>
-<?php
 
-include("connections.php");
-
-    if ($name && $address && $email) {
-
-       $query = mysqli_query($connections, "INSERT INTO mytbl(name,address,email) VALUES('$name','$address','$email')");
-
-       echo "<script language='javascript'>alert('New Record has been inserted!')</script";
-       echo "<script>window.location.href='index.php';</script>";
-
-
-    }
-
-    $view_query = mysqli_query($connections, "SELECT * FROM mytbl");
-
-
-    echo "<table border='1' width='50%'>";
-    echo "<tr>
-            <td>Name</td>
-            <td>Address</td>
-            <td>Email</td>
-
-            <td>Option</td>
-
-         </tr>";
-
-    while($row = mysqli_fetch_assoc($view_query)){
-
-            $user_id = $row["id"];
-
-            $db_name = $row["name"];
-            $db_address = $row["address"];
-            $db_email = $row["email"];
-
-
-            echo "<tr>
-                    <td>$db_name</td>
-                    <td>$db_address</td>
-                    <td>$db_email</td>
-
-                    <td>
-
-                    <a href='Edit.php?id=$user_id'>Update</a>
-
-                    &nbsp;
-
-
-                    < href='ConfirmDelete.php?id=$user_id'>Delete</a>
-
-
-                    </td>
-
-
-
-            </tr>";
-
-
-    }
-
-    echo "</table>";
-?>
-
-<hr>
-
-<?php
-
-
-$Nina = "Nina";
-$Marian = "Marian";
-$Maeren = "Maeren";
-
-$names = array("Nina","Marian","Maeren");
-
-foreach($names as $display_names) {
-
-
-    echo $display_names . "<br>";
-
-
-}
-
-?>
